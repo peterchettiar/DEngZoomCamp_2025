b9b795b7367888261da4096a72d3f797e35a2eda
peter chettiar <peterchet2308@outlook.com>
2024-05-12 12:12:25 +0000

    UPD: New ingestion script for yellow taxi

--- Module-2-orchestration-airflow/airflow/dags_bigquery/data_ingestion_gcs_dag.py
+++ Module-2-orchestration-airflow/airflow/dags_bigquery/data_ingestion_gcs_dag.py
@@ -4,10 +4,10 @@ Transform and Load data into BigQuery
 """
 
 import os
-import logging
+from datetime import datetime
+import pandas as pd
 
 from airflow import DAG
-from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
@@ -15,110 +15,111 @@ from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryCreateExternalTableOperator,
 )
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
 
-PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-BUCKET = os.environ.get("GCP_GCS_BUCKET")
-
-dataset_file = "yellow_tripdata_2021-01.csv"
-dataset_url = f"https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset_file}"
-path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
-parquet_file = dataset_file.replace(".csv", ".parquet")
-BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "trips_data_all")
+# environment variables - if variable is set then "AIRFLOW HOME" else "/opt/airflow/"
+PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+BUCKET = os.getenv("GCP_GCS_BUCKET", "test_bucket_415106")
+BQ_DATASET = os.getenv("BQ_DATASET", "test_dataset")
+AIRFLOW_HOME = os.getenv("AIRFLOW_HOME", "/opt/airflow/")
 
 
-def format_to_parquet(src_file):
-    """
-    Function to convert csv file to parquet
-    """
-    if not src_file.endswith(".csv"):
-        logging.error("Can only accept source files in CSV format, for the moment")
-        return
-    table = pv.read_csv(src_file)
-    pq.write_table(table, src_file.replace(".csv", ".parquet"))
+# templating to parameterise the filename
+URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+URL_TEMPLATE = (
+    URL_PREFIX + "/yellow_tripdata_{{ execution_date.strftime('%Y-%m') }}.parquet"
+)
+
+# make directory in local airflow directory to store all the downloaded raw files
+if not os.path.exists(f"{AIRFLOW_HOME}/raw_data"):
+    os.makedirs(f"{AIRFLOW_HOME}/raw_data")
+
+RAW_FILE_TEMPLATE = (
+    AIRFLOW_HOME
+    + "/raw_data/yellowtaxi_tripdata_{{ execution_date.strftime('%Y-%m') }}.parquet"
+)
+
+# similarly we can create a folder in the airflow directory to hold the processed files
+if not os.path.exists(f"{AIRFLOW_HOME}/cleaned_data"):
+    os.makedirs(f"{AIRFLOW_HOME}/cleaned_data")
+
+CLEAN_FILE_TEMPLATE = (
+    AIRFLOW_HOME
+    + "/cleaned_data/yellowtaxi_tripdata_{{ execution_date.strftime('%Y-%m') }}.parquet"
+)
+
+TABLE_NAME_TEMPLATE = "yellow_taxi_{{ execution_date.strftime('%Y_%m') }}"
 
 
-# NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
-def upload_to_gcs(bucket, object_name, local_file):
-    """
-    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
-    :param bucket: GCS bucket name
-    :param object_name: target path & file-name
-    :param local_file: source path & file-name
-    :return:
-    """
-    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
-    # (Ref: https://github.com/googleapis/python-storage/issues/74)
-    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
-    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
-    # End of Workaround
+# Defining a helper function
+def __clean_file(raw_file_name, clean_file_name):
+    """helper function to do some basic cleaning on file"""
+    # Read the Parquet file into a DataFrame
+    df = pd.read_parquet(raw_file_name)
+    # Fill NaN values with 0 and convert specific columns to int64 data type
+    df = df.fillna(0).astype(
+        {
+            "passenger_count": "int64",
+            "RatecodeID": "int64",
+            "store_and_fwd_flag": "bytes",
+        }
+    )
+    # Save the processed DataFrame as a Parquet file
+    df.to_parquet(clean_file_name, index=False)
 
-    client = storage.Client()
-    bucket = client.bucket(bucket)
 
-    blob = bucket.blob(object_name)
-    blob.upload_from_filename(local_file)
+# Define the function
+def upload_to_gcs(bucket_name, raw_file_name, destination_blob_name, clean_file_name):
+    """Uploads a file to the bucket"""
+    # Create a storage client
+    storage_client = storage.Client()
+    # Get the specified bucket
+    bucket = storage_client.bucket(bucket_name)
+    # Create a blob object with the specified destination blob name
+    blob = bucket.blob(destination_blob_name)
+
+    # running helper function to clean raw file and move to clean file folder
+    __clean_file(raw_file_name, clean_file_name)
+
+    # Upload the cleaned Parquet file to the specified destination blob
+    blob.upload_from_filename(clean_file_name)
 
 
 default_args = {
-    "owner": "airflow",
-    "start_date": days_ago(1),
-    "depends_on_past": False,
+    "start_date": datetime(2021, 1, 1),
+    "end_date": datetime(2024, 2, 1),
     "retries": 1,
 }
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
-    dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@daily",
+    dag_id="ingest_data_bigquery",
+    schedule_interval="0 6 2 * *",
     default_args=default_args,
-    catchup=False,
-    max_active_runs=1,
-    tags=["dtc-de"],
 ) as dag:
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}",
-    )
-
-    format_to_parquet_task = PythonOperator(
-        task_id="format_to_parquet_task",
-        python_callable=format_to_parquet,
-        op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
-        },
+        bash_command=f"curl -sSL {URL_TEMPLATE} > {RAW_FILE_TEMPLATE}",
     )
 
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=upload_to_gcs,
         op_kwargs={
-            "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "bucket_name": BUCKET,
+            "raw_file_name": f"{RAW_FILE_TEMPLATE}",
+            "destination_blob_name": f"yellowtaxi_tripdata/{TABLE_NAME_TEMPLATE}.parquet",
+            "clean_file_name": f"{CLEAN_FILE_TEMPLATE}",
         },
     )
 
     bigquery_external_table_task = BigQueryCreateExternalTableOperator(
         task_id="bigquery_external_table_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_table",
-            },
-            "externalDataConfiguration": {
-                "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
-            },
-        },
+        destination_project_dataset_table=f"{BQ_DATASET}.yellowtaxi_trips",
+        bucket=BUCKET,
+        source_objects=["yellowtaxi_tripdata/*.parquet"],
+        source_format="PARQUET",
     )
 
-(
-    download_dataset_task
-    >> format_to_parquet_task
-    >> local_to_gcs_task
-    >> bigquery_external_table_task
-)
+
+download_dataset_task >> local_to_gcs_task >> bigquery_external_table_task
