df42874ca326ecbff078501c288e84f2d19c6d21
peter chettiar <peterchet2308@outlook.com>
2025-01-30 03:51:41 +0000

    UPD: HW1 2025

---
+++ Module-4-analytics-engineering/taxi_rides_ny/target/compiled/taxi_rides_ny/models/example/my_second_dbt_model.sql
@@ -0,0 +1,5 @@
+-- Use the `ref` function to select from other models
+
+select *
+from `ny-rides-peter-415106`.`dbt_pchettiar`.`my_first_dbt_model`
+where id = 1
---
+++ Module-4-analytics-engineering/taxi_rides_ny/target/compiled/taxi_rides_ny/models/example/schema.yml/not_null_my_first_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from `ny-rides-peter-415106`.`dbt_pchettiar`.`my_first_dbt_model`
+where id is null
+
+
